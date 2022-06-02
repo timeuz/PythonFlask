73410e3c1af73a93526e755cfbe02156cd0e0303
Timeu <timeu.fo@gmail.com>
2022-06-02 11:44:21 -0300

    template

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola(): #função que define o que existe na route
-    return '<h1>Hello World</h1>'
+    return render_template('lista.html')
 
-app.run()
+app.run() # pode ser congigurado para outro host e porta: app.run(host='0.0.0.0', port=8080)
