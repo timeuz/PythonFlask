5c99798057439e474a64d97c0e09c065b85c8275
Timeu <timeu.fo@gmail.com>
2022-06-02 14:54:25 -0300

    ...

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola(): #função que define o que existe na route
-    return render_template('lista.html')
+    
+    jogo1 = Jogo('World of Warcraft', 'MMORPG', 'PC')
+    jogo2 = Jogo('Red Dead Redemption', 'RPG', 'XBOX')
+    jogo3 = Jogo('Age of Empires', 'Estrategia', 'PC')
+    
+    lista_jogos = [jogo1, jogo2, jogo3]
+    
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
 
 app.run() # pode ser congigurado para outro host e porta: app.run(host='0.0.0.0', port=8080)
