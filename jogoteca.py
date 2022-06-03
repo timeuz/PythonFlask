d7ba4dbe192762377b7953c853add1c097995820
Timeu <timeu.fo@gmail.com>
2022-06-03 08:08:41 -0300

    criação de novos jogos

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,21 +6,35 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('World of Warcraft', 'MMORPG', 'PC')
+jogo2 = Jogo('Red Dead Redemption', 'RPG', 'XBOX')
+jogo3 = Jogo('Age of Empires', 'Estrategia', 'PC')
+lista_jogos = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola(): #função que define o que existe na route
-    
-    jogo1 = Jogo('World of Warcraft', 'MMORPG', 'PC')
-    jogo2 = Jogo('Red Dead Redemption', 'RPG', 'XBOX')
-    jogo3 = Jogo('Age of Empires', 'Estrategia', 'PC')
-    
-    lista_jogos = [jogo1, jogo2, jogo3]
-    
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar')
+def criar():
+    #pega dados do form
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    
+    #cria objeto do tipo Jogo
+    jogo = Jogo(nome, categoria, console)
+    
+    #add dados à lista
+    lista_jogos.append(jogo)
+    
+    #mostrando dados da lista_jogos
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
 
 app.run() # pode ser congigurado para outro host e porta: app.run(host='0.0.0.0', port=8080)
